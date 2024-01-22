c6d85fe49f9772193e415479cff8830ff6b86b37
CharMe1001 <stefan.moldoveanu@s.unibuc.ro>
2024-01-22 21:09:01 +0200

    removed empty tests

--- Assets/PlayModeTest/Tester.cs
+++ Assets/PlayModeTest/Tester.cs
@@ -7,22 +7,6 @@ using UnityEngine.TestTools;
 
 public class Tester
 {
-    // A Test behaves as an ordinary method
-    [Test]
-    public void TesterSimplePasses()
-    {
-        // Use the Assert class to test conditions
-    }
-
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator TesterWithEnumeratorPasses()
-    {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
-    }
 
     [UnityTest]
     public IEnumerator TestConnection()
